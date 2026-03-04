0f9f6701ff717e4d2800d27121960abfe0e2af9d
Rob Pungello <rob@rpun.io>
2026-03-04 11:03:20 -0500

    Add livewire titles

--- app/Livewire/ListIncidents.php
+++ app/Livewire/ListIncidents.php
@@ -6,9 +6,11 @@ use App\Models\Incident;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Contracts\View\View;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 use Livewire\WithPagination;
 
+#[Title('Incidents')]
 class ListIncidents extends Component
 {
     use WithPagination;
--- app/Livewire/ListTargetGroups.php
+++ app/Livewire/ListTargetGroups.php
@@ -6,8 +6,10 @@ use App\Models\TargetGroup;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Target Groups')]
 class ListTargetGroups extends Component
 {
     public function render(): View
